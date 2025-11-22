bb1d85b063c34f1278e935d49858d0dbc743f2b4
ahmadalshawakri <ashawakri@gmail.com>
2025-11-23 01:49:19 +0300

    Add redirect from root to /login in next.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  // Default redirect to login page
-  // Auth will be handled by login page and dashboard layout
-  redirect('/login');
+export default function RootPage() {
+  redirect("/login");
 }
